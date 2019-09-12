434724554386dc2f85558ce5e6f822907b8d2e1d
msimbao <32774483+msimbao@users.noreply.github.com>
2019-09-11 20:57:17 -0500

    Add files via upload

---
+++ lda_bible_visualizer/lda.py
@@ -0,0 +1,217 @@
+import re
+
+'''
+Loading Gensim and nltk libraries
+'''
+# pip install gensim
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+np.random.seed(400)
+
+from getImages import prepare_string
+
+import nltk
+
+from google_images_download import google_images_download  
+  
+# creating object 
+response = google_images_download.googleimagesdownload()  
+  
+
+  
+  
+def downloadimages(query): 
+    # keywords is the search query 
+    # format is the image file format 
+    # limit is the number of images to be downloaded 
+    # print urs is to print the image file url 
+    # size is the image size which can 
+    # be specified manually ("large, medium, icon") 
+    # aspect ratio denotes the height width ratio 
+    # of images to download. ("tall, square, wide, panoramic") 
+    arguments = {"keywords": query, 
+                 "format": "jpg", 
+                 "limit":2, 
+                 "print_urls":True, 
+                 "size": "medium", 
+                 "aspect_ratio": "panoramic",
+                 "no_directory":"images",
+                 } 
+    try: 
+        response.download(arguments) 
+      
+    # Handling File NotFound Error     
+    except FileNotFoundError:  
+        arguments = {"keywords": query, 
+                     "format": "jpg", 
+                     "limit":2, 
+                     "print_urls":True,  
+                     "size": "medium"} 
+                       
+        # Providing arguments for the searched query 
+        try: 
+            # Downloading the photos based 
+            # on the given arguments 
+            response.download(arguments)  
+        except: 
+            pass
+  
+# Driver Code 
+
+
+def train_lda():
+    nltk.download('wordnet')
+
+
+    print(WordNetLemmatizer().lemmatize('went', pos = 'v')) # past tense to present tense
+
+
+    import pandas as pd
+    stemmer = SnowballStemmer("english")
+    original_words = ['caresses', 'flies', 'dies', 'mules', 'denied','died', 'agreed', 'owned', 
+            'humbled', 'sized','meeting', 'stating', 'siezing', 'itemization','sensational', 
+            'traditional', 'reference', 'colonizer','plotted']
+    singles = [stemmer.stem(plural) for plural in original_words]
+
+    pd.DataFrame(data={'original word':original_words, 'stemmed':singles })
+
+    '''
+    Write a function to perform the pre processing steps on the entire dataset
+    '''
+    def lemmatize_stemming(text):
+        return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+    # Tokenize and lemmatize
+    def preprocess(text):
+        result=[]
+        for token in gensim.utils.simple_preprocess(text) :
+            if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+                result.append(lemmatize_stemming(token))
+                
+        return result
+
+
+    '''
+    Preview a document after preprocessing
+    '''
+
+    file = open('revelation.txt','r')
+
+    processed_docs = []
+
+    for doc in file:
+        processed_docs.append(preprocess(doc))
+
+        
+    '''
+    Create a dictionary from 'processed_docs' containing the number of times a word appears 
+    in the training set using gensim.corpora.Dictionary and call it 'dictionary'
+    '''
+    dictionary = gensim.corpora.Dictionary(processed_docs)
+
+    '''
+    Checking dictionary created
+    '''
+    count = 0
+    for k, v in dictionary.iteritems():
+        print(k, v)
+        count += 1
+        if count > 10:
+            break
+
+    dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
+
+    bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+
+    '''
+    Preview BOW for our sample preprocessed document
+    '''
+    document_num = 20
+    bow_doc_x = bow_corpus[document_num]
+
+    for i in range(len(bow_doc_x)):
+        print("Word {} (\"{}\") appears {} time.".format(bow_doc_x[i][0], 
+                                                        dictionary[bow_doc_x[i][0]], 
+                                                        bow_doc_x[i][1]))
+
+
+    # LDA mono-core -- fallback code in case LdaMulticore throws an error on your machine
+    # lda_model = gensim.models.LdaModel(bow_corpus, 
+    #                                    num_topics = 10, 
+    #                                    id2word = dictionary,                                    
+    #                                    passes = 50)
+
+    # LDA multicore 
+    '''
+    Train your lda model using gensim.models.LdaMulticore and save it to 'lda_model'
+    '''
+    # TODO
+    lda_model =  gensim.models.LdaMulticore(bow_corpus, 
+                                    num_topics = 8, 
+                                    id2word = dictionary,                                    
+                                    passes = 10,
+                                    workers = 2)
+
+
+    '''
+    For each topic, we will explore the words occuring in that topic and its relative weight
+    '''
+    for idx, topic in lda_model.print_topics(-1):
+        print("Topic: {} \nWords: {}".format(idx, topic ))
+        print("\n")
+
+    """
+    Getting Independent data using beautiful soup
+    """
+
+    from bs4 import BeautifulSoup
+    from bs4.element import Comment
+    import urllib.request
+
+    def tag_visible(element):
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        elif isinstance(element,Comment):
+            return False
+        elif re.match(r"[\s\r\n]+",str(element)): 
+            return False
+        return True
+
+
+    def text_from_html(body):
+        soup = BeautifulSoup(body, 'html.parser')
+        texts = soup.findAll(text=True)
+        visible_texts = filter(tag_visible, texts)  
+        return u" ".join(t.strip() for t in visible_texts)
+
+    html = urllib.request.urlopen('https://www.biblegateway.com/passage/?search=Revelation+11&version=KJV').read()
+    text = text_from_html(html)
+
+    bow_vector = dictionary.doc2bow(preprocess(text))
+
+    ldaBowList = []
+    topicList = []
+
+    for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
+        text = "Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5))
+        ldaBowList.append(re.findall(r'"(.*?)"', text))
+
+    for item in ldaBowList:
+        for topic in item:
+            topicList.append(topic)
+
+    print(topicList)
+
+    for topic in topicList: 
+        topic = "bible revelation" + topic
+        downloadimages(topic)  
+        print()
+
+
+
+    
+
---
+++ lda_bible_visualizer/main.py
@@ -0,0 +1,91 @@
+'''
+Basic Picture Viewer
+====================
+
+This simple image browser demonstrates the scatter widget. You should
+see three framed photographs on a background. You can click and drag
+the photos around, or multi-touch to drop a red dot to scale and rotate the
+photos.
+
+The photos are loaded from the local images directory, while the background
+picture is from the data shipped with kivy in kivy/data/images/background.jpg.
+The file pictures.kv describes the interface and the file shadow32.png is
+the border to make the images look like framed photographs. Finally,
+the file android.txt is used to package the application for use with the
+Kivy Launcher Android application.
+
+For Android devices, you can copy/paste this directory into
+/sdcard/kivy/pictures on your Android device.
+
+The images in the image directory are from the Internet Archive,
+`https://archive.org/details/PublicDomainImages`, and are in the public
+domain.
+
+'''
+
+import kivy
+kivy.require('1.0.6')
+
+from glob import glob
+from random import randint
+from os.path import join, dirname
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.scatter import Scatter
+from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
+from kivy.core.text import LabelBase
+
+LabelBase.register(name="Fredoka",
+    fn_regular= "fonts/FredokaOne-Regular.ttf"
+    )
+
+LabelBase.register(name="Goth",
+    fn_regular= "fonts/CocoGothic-UltraLight_trial.ttf"
+    )
+
+
+from lda import train_lda
+from getImages import prepare_string
+train_lda()
+
+class Picture(Scatter):
+    '''Picture is the class that will show the image with a white border and a
+    shadow. They are nothing here because almost everything is inside the
+    picture.kv. Check the rule named <Picture> inside the file, and you'll see
+    how the Picture() is really constructed and used.
+
+    The source property will be the filename to show.
+    '''
+
+    source = StringProperty(None)
+
+
+class MainApp(App):
+    passage = ObjectProperty(None)
+
+    def predict(self):
+        pass
+
+    def build(self):
+
+        # the root is created in pictures.kv
+        root = self.root
+        
+        # get any files into images directory
+        curdir = dirname(__file__)
+        for filename in glob(join(curdir, 'downloads', '*')):
+            try:
+                # load the image
+                picture = Picture(source=filename, rotation=randint(-30, 30))
+                # add to the main field
+                root.add_widget(picture)
+            except Exception as e:
+                Logger.exception('Pictures: Unable to load <%s>' % filename)
+
+    def on_pause(self):
+        return True
+
+
+if __name__ == '__main__':
+    MainApp().run()
